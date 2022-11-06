dec624444b8cf03e61e5f365d7e3688c7c80e649
Pierre <pierre.kutnik@gmail.com>
2022-11-06 18:24:36 +0100

    dodano typy obiektowe

---
+++ src/TypyObiektowe.java
@@ -0,0 +1,17 @@
+public class TypyObiektowe {
+    //typy obiektowe przechowuja wartosc i moga wykonywac metody
+    public static void main(String[] args) {
+        String hello = "Hello";
+
+        System.out.println(" Czy zawiera o  :" + " " +hello.endsWith("o"));
+
+        System.out.println(hello.charAt(0));
+
+      Integer firstnumber = 3245;
+
+      firstnumber.doubleValue();
+        System.out.println(  firstnumber.doubleValue());
+
+
+    }
+}
