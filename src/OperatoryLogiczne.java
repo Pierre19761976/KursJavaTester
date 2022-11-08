eb4c9309b2777add869d71ced56ad5fcf31075ce
Pierre <pierre.kutnik@gmail.com>
2022-11-08 21:16:46 +0100

    add OperatoryLogiczne.java

--- src/OperatoryLogiczne.java
+++ src/OperatoryLogiczne.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class OperatoryLogiczne {
     public static void main(String[] args) {
-        boolean firstValue = true;
+    /*    boolean firstValue = true;
         boolean secondValue = false;
         boolean thirdValue = false;
         boolean fourthValue = true;
@@ -17,7 +19,19 @@ public class OperatoryLogiczne {
         System.out.println(firstValue && !secondValue);
         System.out.println(!(firstValue&&secondValue));
         boolean fifthValvue = 2 < 1;
-        System.out.println(fifthValvue);
+        System.out.println(fifthValvue);*/
+
+        Scanner scanner =new Scanner(System.in);
+                System.out.println("Prosze podaj liczbe a");
+        int a = scanner.nextInt();
+        System.out.println("Prosze podaj liczbe b");
+        int b = scanner.nextInt();
+        boolean firstValue = a > b;
+
+
+        System.out.println("Liczba a jest wieksza od b: " + firstValue);
+
+
 
     }
 }
