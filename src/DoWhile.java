4527ada6f8d4e739461ff72e191ad12128f57cff
Pierre <pierre.kutnik@gmail.com>
2022-11-11 21:59:06 +0100

    add DoWhile.java

---
+++ src/DoWhile.java
@@ -0,0 +1,14 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int number = 5;
+
+        do { System.out.println("Liczba" + " " + number + " " + "jest mniejsza od 20");
+            number++;
+
+        } while (number<20);
+
+    }
+}
+
+
+
