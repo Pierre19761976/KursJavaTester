923776821a5089f97a67493c95f59faec00b9625
Pierre <pierre.kutnik@gmail.com>
2022-11-11 21:58:42 +0100

    add PetlaWhile.java

---
+++ src/PetlaWhile.java
@@ -0,0 +1,9 @@
+public class PetlaWhile {
+    public static void main(String[] args) {
+        int number=10;
+        while (number<20) {
+            System.out.println("Liczba" + " " + number + " " + "jest mniejsza od 20");
+            number++;
+        }
+    }
+}
