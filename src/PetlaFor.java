363da4175d967f1b44427480e400385eb69dc14e
Pierre <pierre.kutnik@gmail.com>
2022-11-11 21:28:50 +0100

    add PetlaFor.java

---
+++ src/PetlaFor.java
@@ -0,0 +1,28 @@
+public class PetlaFor {
+    public static void main(String[] args) {
+        //np wypisanie okreslonej liczby liczb np od 0-100
+        //co druga liczbe
+        // int number = 100;
+        // i specjalna zmienna sterujaca
+
+        // for (int i = 0; i < 100; i = i + 2) {
+        // i ustawia wartosc poczatkowa i = 0
+        //pozniej warunek do ktorego momentu ma sie wykonywac
+        // 3 czlon jak to bedzie sie zmienialo
+
+        //  System.out.println(i);
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
